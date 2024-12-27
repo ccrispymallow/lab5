c078a7266404d372f00386e775350b8ff7359ee5
morun <162666897+ccrispymallow@users.noreply.github.com>
2024-12-27 17:21:48 +0700

    5_1_2.py (window2)

--- 5_1_2.py
+++ 5_1_2.py
@@ -7,28 +7,16 @@ class Simple_drawing_window2(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple GitHub Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.cat = QPixmap("images\cat.webp")
 
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
 
-        p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-            QPoint(70, 100), QPoint(100, 110),
-            QPoint(130, 100), QPoint(100, 150)
-        ])
+        p.drawPixmap(QRect(200, 100, 320, 320), self.cat)
 
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
-
-        p.drawPolygon([QPoint(50, 200), QPoint(150, 200), QPoint(100, 400)])
-        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
-
-        p.setPen(QColor(127, 127, 255))  
-        p.setBrush(QColor(127, 127, 255))  
+        p.setPen(QColor(128, 128, 128))  
+        p.setBrush(QColor(128, 128, 128))  
         p.drawRect(50, 50, 100, 100)  
 
         p.end()
